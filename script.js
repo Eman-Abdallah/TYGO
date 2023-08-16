e0ba6e4584f68e39c80ae82144c743c897686813
Eman Abdallah <emanshaltoot623@gmail.com>
2023-08-16 10:48:48 +0300

    add blog page

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ function myFunction() {
     }
 
 }
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+        500: {
+            slidesPerView: 2,
+            spaceBetween: -10,
+        },
+        700: {
+            slidesPerView: 2,
+            spaceBetween: -30,
+        },
+        900:{
+            slidesPerView: 3,
+            spaceBetween: -30,
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-button-next ",
+        prevEl: ".swiper-button-prev ",
+    },
+  })
